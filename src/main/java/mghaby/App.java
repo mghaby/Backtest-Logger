507c4146283728b51c100c9dcb63b261349d8888
mghaby <mark.ghaby71@hotmail.com>
2021-12-28 14:27:29 +1100

    pseudocode cleanup in app
    pseudocode cleanup in app

--- src/main/java/mghaby/App.java
+++ src/main/java/mghaby/App.java
@@ -74,31 +74,8 @@ public class App {
          */         // this prints to db.json in main directory for some reason
     }
 
-    // methods here
-
-
-
-
-
-
-
-
-
+    // methods here if needed
 
 }
 
-// essnetialy this should mimic stevens excel spreadsheet but act as an interface for it and the database.
-
-// +--------------------------------------------------------------------------+
-// logger class
-// communicator class (file io to the DB class)
-
-// +--------------------------------------------------------------------------+
-// in the logger class you need to take starting account balance, pair and risk % per position.
-// then you need this class to need to have calcualtion methods called upon this class.
-// each instance of logger should be pushed to the database itself and should be treated as a single instance
-
 // use jframe and jbuttons to add trades to the logger (let it be ugly until it is done)
-
-// after this you may choose to make a starting interface that will allow the user to access one of the exisitng loggers or make a new one
-
